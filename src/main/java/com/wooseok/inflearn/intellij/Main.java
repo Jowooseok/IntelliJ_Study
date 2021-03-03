3318a60576dda995b80180aea07ff068181f2d18
DESKTOP-48B4POT\PLANIT <whdntjrsla12@naver.com>
2021-03-03 16:48:27 +0900

    #라인 수정하기

--- src/main/java/com/wooseok/inflearn/intellij/Main.java
+++ src/main/java/com/wooseok/inflearn/intellij/Main.java
@@ -7,6 +7,22 @@ public class Main {
         System.out.println("aaa");
         //실행방법 shift+ctrl+f10
 
+        //라인 복제 ctrl+d
+        //라인 삭제 ctrl+y
+
+        //라인 합치기 ctrl+shift+j
+        String query = "SELECT * " +
+                "FROM member " +
+                "WHERE member.name = 'wooseok";
+
+        //라인 옮기기
+        //1.구문이동 ctrl+shift+up or down
+        //2.라인이동 alt+shift+up or down
+        System.out.println("옮길것");
+        System.out.println("=========");
+        //3. 엘리먼트 단위로 html에서 사용 alt+ctrl+shift + left or right
+
+
 
     }
 }
